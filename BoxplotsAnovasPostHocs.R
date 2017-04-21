50160665d183a4114bcdcfea209db99aed068da0
jon-may <jon.may@plymouth.ac.uk>
2017-04-21 14:41:33 +0100

    Add files via upload

--- BoxplotsAnovasPostHocs.R
+++ BoxplotsAnovasPostHocs.R
@@ -5,73 +5,67 @@ library(readr)
 artdata <- read_csv("https://github.com/jon-may/multi-aesthetics/raw/master/artdatacsv.csv", 
      col_types = cols(Piece = col_factor(levels = c("Tree", "Plate", "Smooth", "Rough")), Condition = col_factor(levels = c("Combo","Sound","Object"))))
 
-# create three subframes, one for each condition
-Cond<-split(artdata, artdata$Condition)
-# get descriptives for each adjective separately for Object and Combo
-ComboDesc<-describeBy(Cond$Combo[,6:19], group = Cond$Combo$Piece, mat=FALSE)
-SoundDesc<-describeBy(Cond$Sound[,6:19], group = Cond$Sound$Piece, mat=FALSE)
-ObjectDesc<-describeBy(Cond$Object[,6:19], group = Cond$Object$Piece, mat=FALSE)
 
 # create function to do boxplots, anova and posthocs for any variable
-myAnalysis <- function(x){
+myAnalysis <- function(x, g){
   varname <-deparse(substitute(x)) # obtain variable name for titling plot
-  boxplot(x ~ Condition, notch=TRUE, main=paste(varname))
-  model<-aov(x ~ Condition) # oneway anova of variable by Condition
-  summary(model) # anova table output for main effect of Condition
-  TukeyHSD(model) # posthocs for levels of Condition
+  boxplot(x ~ g, notch=TRUE, main=paste(varname))
+  model<-aov(x ~ g) # oneway anova of variable by Condition
+  print(summary(model)) # anova table output for main effect of Condition
+  print(describeBy(x, group=g))
+  print(TukeyHSD(model)) # posthocs for levels of Condition
   }
 
 
 tree <- subset(artdata, artdata$Piece=="Tree")
-attach(tree)
 # first two are OBJECT descriptors so should be LOWER for sound
-myAnalysis(Expanding)
-myAnalysis(Textured)
+myAnalysis(tree$Expanding, tree$Condition)
+myAnalysis(tree$Textured, tree$Condition)
 # next two are SOUND descriptors so should be LOWER for object
-myAnalysis(Metallic)
-myAnalysis(Warping  )
+myAnalysis(tree$Metallic, tree$Condition)
+myAnalysis(tree$Warping  , tree$Condition)
 # next two are COMBO descriptors so should be HIGHER for combo
-myAnalysis(Moving)
-myAnalysis(Chronological  )
-detach(tree)
+myAnalysis(tree$Moving, tree$Condition)
+myAnalysis(tree$Chronological  , tree$Condition)
+
 
 plate <- subset(artdata, artdata$Piece=="Plate")
-attach(plate)
+
 # first two are OBJECT descriptors so should be LOWER for sound
-myAnalysis(Travelling)
-myAnalysis(Mechanical)
+myAnalysis(plate$Travelling, plate$Condition)
+myAnalysis(plate$Mechanical, plate$Condition)
 # next two are SOUND descriptors so should be LOWER for object
-myAnalysis(Detailed)
-myAnalysis(Near  )
+myAnalysis(plate$Detailed, plate$Condition)
+myAnalysis(plate$Near  , plate$Condition)
 # next two are COMBO descriptors so should be HIGHER for combo
-myAnalysis(Scratching)
-myAnalysis(Light  )
-detach(plate)
+myAnalysis(plate$Scratching, plate$Condition)
+myAnalysis(plate$Light  , plate$Condition)
+
 
 smooth <- subset(artdata, artdata$Piece=="Smooth")
-attach(smooth)
+
 # first two are OBJECT descriptors so should be LOWER for sound
-myAnalysis(Static)
-myAnalysis(Moist)
+myAnalysis(smooth$Static, smooth$Condition)
+myAnalysis(smooth$Moist, smooth$Condition)
 # next two are SOUND descriptors so should be LOWER for object
-myAnalysis(Hollow)
-myAnalysis(Unfinished  )
+myAnalysis(smooth$Hollow, smooth$Condition)
+myAnalysis(smooth$Unfinished  , smooth$Condition)
 # next two are COMBO descriptors so should be HIGHER for combo
-myAnalysis(Alive)
-myAnalysis(Threatening  )
-detach(smooth)
+myAnalysis(smooth$Alive, smooth$Condition)
+myAnalysis(smooth$Threatening  , smooth$Condition)
+
 
 rough <- subset(artdata, artdata$Piece=="Rough")
-attach(rough)
+
 # first two are OBJECT descriptors so should be LOWER for sound
-myAnalysis(Violent)
-myAnalysis(Engulfing)
+myAnalysis(rough$Violent, rough$Condition)
+myAnalysis(rough$Engulfing, rough$Condition)
 # next two are SOUND descriptors so should be LOWER for object
-myAnalysis(Spiky)
-myAnalysis(Chaotic  )
+myAnalysis(rough$Spiky, rough$Condition)
+myAnalysis(rough$Chaotic  , rough$Condition)
 # next two are COMBO descriptors so should be HIGHER for combo
-myAnalysis(Aggressive)
-myAnalysis(Dynamic)
-detach(rough)
+myAnalysis(rough$Aggressive, rough$Condition)
+myAnalysis(rough$Dynamic, rough$Condition)
+
 
 
